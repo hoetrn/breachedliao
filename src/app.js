9317330ba51e001c54475c53b83a64528b073c3d
Nat <hoetrn@hotmail.com>
2025-05-23 01:47:37 +0000

    report fix

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const path = require("path");
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const fetch = require("node-fetch");
